c25dba0be8c0108819a9fe0d23240361e08230be
shadrach akaade <akaadesm@gmail.com>
2023-01-19 20:03:08 +0100

    remove wrong import

--- src/components/about/resume.jsx
+++ src/components/about/resume.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { GiTargetArrows } from 'react-icons/gi'
-import { Link } from 'react-router-dom'
-import resume from '/Users/User/projects/portfolio/src/assets/Resume(Frontend).pdf'
 
 const Resume = () => {
   return (
