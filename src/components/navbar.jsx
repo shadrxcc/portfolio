7a207c8531de81840d887083ec4abb1f02254369
shadrach akaade <akaadesm@gmail.com>
2023-01-19 19:48:10 +0100

    append routes to nav links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@ import { React, Component } from "react";
 import logo from "../assets/logo.svg";
 import menu from "../assets/menu.svg";
 import { GrClose } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   state = {
@@ -18,7 +19,7 @@ class Navbar extends Component {
       <div className="">
         <header className="transition-all py-0 px-[1.5rem] md:px-[3rem] flex justify-between items-center ease-in duration-300 w-full fixed h-20">
           <div>
-            <img src={logo} alt="logo" />
+            <Link to='/'><img src={logo} alt="logo" /></Link>
           </div>
           <nav>
             <ul
@@ -27,16 +28,15 @@ class Navbar extends Component {
                 this.state.clicked ? "#navbar active" : "#navbar"
               } bg-[rgba(23, 23, 23)] text-white flex flex-col mt-[15em] gap-[2em] md:mt-0 items-center md:bg-inherit bg-[rgba(23, 23, 23)] w-[100%] right-[0px] h-[100vh] fixed md:relative md:top-0 top-[-100em] md:flex-row`}
             >
-              <li className="active">
-                <a href="/">Projects</a>
-              </li>
-              <li className="active">
-                <a href="/">About</a>
-              </li>
-              <li className="active">
-                <a href="/">Contact</a>
-              </li>
-              <button></button>
+             <Link to='/projects'><li className="active">
+                Projects
+              </li></Link>
+               <Link to='/about'><li className="active">
+                About
+              </li></Link>
+               <Link to='/contact'><li className="active">
+                Contact
+              </li></Link>
             </ul>
           </nav>
           <div
