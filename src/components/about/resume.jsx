43701933ec830b0c2c4e42df558b2135e5e72960
shadrach akaade <akaadesm@gmail.com>
2023-01-21 17:49:30 +0100

    add animation to get resume

--- src/components/about/resume.jsx
+++ src/components/about/resume.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const Resume = () => {
   return (
     <>
+    <div>
+      <a className="" href="/"><p className='resume text-lg text-white'>Get my resume</p></a>
+    </div>
     
     </>
   )
