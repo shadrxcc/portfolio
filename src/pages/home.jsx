db0a045a9846c2d03d792e2a130fbc72230194fe
shadrach akaade <akaadesm@gmail.com>
2023-02-04 18:48:05 +0100

    change card title color

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,21 +14,21 @@ const Home = () => {
         animate={{ y: "0%", opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.75, ease: "easeInOut" }}
-        className="mt-[5rem] flex flex-col gap-y-7"
+        className="mt-[5rem] flex flex-col gap-y-9"
       >
         <div className="mx-[24px] md:w-[70%]">
           <div className="">
-            <Animatedintro text={"Hello, i'm"} />
+            <Animatedintro text={"Hello, I'm"} />
             <Animatedname text={"Shadrach Akaade"} />
             <Animatedtitle text={"Frontend Engineer"} />
           </div>
           <div>
             <Animatedsummary
-              text={`I create aesthetically pleasing, accessible, and functional user-friendly web experiences.`}
+              text={`I create aesthetically pleasing, accessible, and functional web experiences.`}
             />
           </div>
         </div>
-        <HighlightedProjects />
+        <HighlightedProjects/>
       </motion.div>
     </>
   );
