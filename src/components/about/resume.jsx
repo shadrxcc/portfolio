1147bbbe6258b7fed46df6094f08490122844c82
shadrxcc <akaadesm@gmail.com>
2023-07-26 15:44:21 +0100

    change resume file

--- src/components/about/resume.jsx
+++ src/components/about/resume.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import resume from "./Resume(Frontend).pdf";
+import resume from "./Résumé.pdf";
 import { BsArrowRight } from "react-icons/bs";
 
 const Resume = () => {
   return (
-    
     <>
       <div>
-        <a className=" flex gap-x-2 items-center"  target="_blank" rel="noreferrer" href={`https://docs.google.com/document/d/1ceAZmZYJo2wn3evUjF8ofIJDeCFi4zXfE0zoopJmUcE/edit?usp=sharing`} download>
-          <p className="resume md:text-base text-lg text-white opacity-75 hover:opacity-100">
-            Get my resume{" "}
-          </p>
-          <BsArrowRight className="resume text-white" />
+        <a
+          className=" flex gap-x-2 items-center"
+          target="_blank"
+          rel="noreferrer"
+          href={resume}
+          download
+        >
+          <p className="text hover:underline text-neutral-400">Get my Résumé</p>
         </a>
       </div>
     </>
